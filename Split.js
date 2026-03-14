a1489e31b593d2c4cf067c285d840a7d1de181d5
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-14 16:22:27 +0530

    split

---
+++ Split.js
@@ -0,0 +1,7 @@
+function onClicksplit(){
+    debugger;
+    number=(document.getElementById("txtnumber").value);
+    Amount=(document.getElementById("txtAmount").value);
+    Average=Amount/number;
+    document.getElementById("pResult").innerHTML=Average
+}
