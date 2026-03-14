bc4c2198b62908ccd31d8333d4d74d2c15926782
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-14 17:46:56 +0530

    bottles html

--- BottleCalculations.js
+++ BottleCalculations.js
@@ -1,4 +1,7 @@
 function onClickBottle(){
     debugger;
-    
+    liters=(document.getElementById("txtliters").value);
+    bottles= liters/1.5
+    document.getElementById("pResult").innerHTML=Math.ceil(bottles)
+
 }
