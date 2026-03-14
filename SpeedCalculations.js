1f81c84fc956ba934401c7157672938883d700fd
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-14 16:53:41 +0530

    feat(SpeedCalculations.) built in html

--- SpeedCalculations.js
+++ SpeedCalculations.js
@@ -1,1 +1,11 @@
-function 
+function onClickSpeed(){
+    debugger;
+    time=document.getElementById(txtTime).value;
+    distance=document.getElementById(txtDistance).value;
+    timeINHR=Time/60;
+    distanceINKM=Distance/1000;
+    speed=Distance/Time;
+    document.getElementByI("pResult").innerHTML=Speed
+
+
+}
