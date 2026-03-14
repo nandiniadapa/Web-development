eb399426e39fa3c0ea6f1603e4ed68082f96dab5
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-14 17:11:32 +0530

    feat(SpeedCalculations) in js code

--- SpeedCalculations.js
+++ SpeedCalculations.js
@@ -1,11 +1,11 @@
 function onClickSpeed(){
     debugger;
-    time=document.getElementById(txtTime).value;
-    distance=document.getElementById(txtDistance).value;
-    timeINHR=Time/60;
-    distanceINKM=Distance/1000;
-    speed=Distance/Time;
-    document.getElementByI("pResult").innerHTML=Speed
+    time=(document.getElementById("txtTime").value)
+    distance=(document.getElementById("txtDistance").value);
+    timeINHR=time/60;
+    distanceINKM=distance/1000;
+    speed=distanceINKM/timeINHR;
+    document.getElementById("pResult").innerHTML=speed +"km/hr"
 
 
 }
