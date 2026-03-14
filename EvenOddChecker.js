c4d658f7d02308cec4d1604760bc94cbd106840b
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-14 12:51:39 +0530

    even odd checker

---
+++ EvenOddChecker.js
@@ -0,0 +1,16 @@
+function onClickCheckEvenOddNumber(){
+    debugger;
+ var  number= parseInt(document.getElementById("txtNumber").value);
+ if (number%2==0) {
+    document.getElementById("pResult").innerHTML= number+ " is Even Number" ;
+
+ }
+  else;
+
+{
+  document.getElementById("pResult").innerHTML= number+ " is Odd Number" ;
+
+ }
+}
+
+
